99ebce5cfba3c3d2eeb3816408f64b86dd239cc2
Steve Barrios <hsteve1992@yahoo.com>
2020-07-18 20:50:55 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Hector Barrios on 7/18/20.
+//  Copyright © 2020 Hector Barrios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
